447c9896d162ec534231d3aeca1e29cc13d601f2
JasonLin8888 <jasonlin@college.harvard.edu>
2024-01-16 15:40:10 -0500

    Fixing errors after merging code from Henry

--- app/org-catalog/org-detail-dialog.tsx
+++ app/org-catalog/org-detail-dialog.tsx
@@ -1,40 +1,69 @@
-'use client';
-import { Orgs } from "@/lib/firebase/schema";
+"use client";
+import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button"
 import { TypographyP } from "@/components/ui/typography";
-
+import type { Orgs } from "@/lib/firebase/schema";
 
 export default function OrgDetailDialog(org: Orgs) {
-    return (
-        <Dialog>
-        <DialogTrigger asChild>
-            <Button variant="outline">Learn More</Button>
-        </DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-            <DialogTitle>{org.name}</DialogTitle>
-            <DialogDescription>
-                <TypographyP><b>Description:</b> {org.description}</TypographyP>
-                <TypographyP><b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
-                <TypographyP><b>Members:</b> {org.members.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
-                <TypographyP><b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a></TypographyP>
-                <TypographyP><b>Type:</b> {org.type}</TypographyP>
-                <TypographyP><b>Comp Type:</b> {org.comptype}</TypographyP>
-                <TypographyP><b>Meeting Time:</b> {org.meetingtime}</TypographyP>               
-                <TypographyP><b>Time Commitment:</b> {org.timecommitment}</TypographyP>
-                <Button variant="outline" onClick={() => { alert('Edit button clicked') }}>Edit</Button>
-                <Button variant="outline" onClick={() => { alert('Delete button clicked') }}>Delete</Button>
-            </DialogDescription>
-            </DialogHeader>
-        </DialogContent>
-        </Dialog>
-    )
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Learn More</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{org.name}</DialogTitle>
+          <DialogDescription>
+            <TypographyP>
+              <b>Description:</b> {org.description}
+            </TypographyP>
+            <TypographyP>
+              <b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ", " + b)}
+            </TypographyP>
+            <TypographyP>
+              <b>Members:</b> {org.members.reduce((a: string, b: string) => a + ", " + b)}
+            </TypographyP>
+            <TypographyP>
+              <b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a>
+            </TypographyP>
+            <TypographyP>
+              <b>Type:</b> {org.type}
+            </TypographyP>
+            <TypographyP>
+              <b>Comp Type:</b> {org.comptype}
+            </TypographyP>
+            <TypographyP>
+              <b>Meeting Time:</b> {org.meetingtime}
+            </TypographyP>
+            <TypographyP>
+              <b>Time Commitment:</b> {org.timecommitment}
+            </TypographyP>
+            <Button
+              variant="outline"
+              onClick={() => {
+                alert("Edit button clicked");
+              }}
+            >
+              Edit
+            </Button>
+            <Button
+              variant="outline"
+              onClick={() => {
+                alert("Delete button clicked");
+              }}
+            >
+              Delete
+            </Button>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }

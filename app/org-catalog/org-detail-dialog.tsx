c3a7e8bf709f3906fc9be203fc0076eb5ce292e2
Henry Wu <hhwu@college.harvard.edu>
2024-01-17 02:10:42 -0500

    Added org detail dialog to appropriate pages, included edit/delete button modality
    Co-authored-by: JasonLin8888 <JasonLin8888@users.noreply.github.com>

--- app/org-catalog/org-detail-dialog.tsx
+++ app/org-catalog/org-detail-dialog.tsx
@@ -9,61 +9,37 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { TypographyP } from "@/components/ui/typography";
-import type { Orgs } from "@/lib/firebase/schema";
+import { Orgs } from "@/lib/firebase/schema";
 
-export default function OrgDetailDialog(org: Orgs) {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Learn More</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{org.name}</DialogTitle>
-          <DialogDescription>
-            <TypographyP>
-              <b>Description:</b> {org.description}
-            </TypographyP>
-            <TypographyP>
-              <b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ", " + b)}
-            </TypographyP>
-            <TypographyP>
-              <b>Members:</b> {org.members.reduce((a: string, b: string) => a + ", " + b)}
-            </TypographyP>
-            <TypographyP>
-              <b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a>
-            </TypographyP>
-            <TypographyP>
-              <b>Type:</b> {org.type}
-            </TypographyP>
-            <TypographyP>
-              <b>Comp Type:</b> {org.comptype}
-            </TypographyP>
-            <TypographyP>
-              <b>Meeting Time:</b> {org.meetingtime}
-            </TypographyP>
-            <TypographyP>
-              <b>Time Commitment:</b> {org.timecommitment}
-            </TypographyP>
-            <Button
-              variant="outline"
-              onClick={() => {
-                alert("Edit button clicked");
-              }}
-            >
-              Edit
-            </Button>
-            <Button
-              variant="outline"
-              onClick={() => {
-                alert("Delete button clicked");
-              }}
-            >
-              Delete
-            </Button>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+
+interface OrgDetailDialogProps {
+    org: Orgs;
+    cardEditsVisible: boolean;
+}
+
+export default function OrgDetailDialog({ org, cardEditsVisible }: OrgDetailDialogProps) {
+    return (
+        <Dialog>
+        <DialogTrigger asChild>
+            <Button variant="outline">Learn More</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+            <DialogTitle>{org.name}</DialogTitle>
+            <DialogDescription>
+                <TypographyP><b>Description:</b> {org.description}</TypographyP>
+                <TypographyP><b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
+                <TypographyP><b>Members:</b> {org.members.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
+                <TypographyP><b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a></TypographyP>
+                <TypographyP><b>Type:</b> {org.type}</TypographyP>
+                <TypographyP><b>Comp Type:</b> {org.comptype}</TypographyP>
+                <TypographyP><b>Meeting Time:</b> {org.meetingtime}</TypographyP>               
+                <TypographyP><b>Time Commitment:</b> {org.timecommitment}</TypographyP>
+                {cardEditsVisible && <Button id="editButton" variant="outline" onClick={() => { alert('Edit button clicked') }}>Edit</Button>}
+                {cardEditsVisible && <Button id="deleteButton" variant="outline" onClick={() => { alert('Delete button clicked') }}>Delete</Button>}
+            </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+        </Dialog>
+    )
 }
--- app/org-catalog/page.tsx
+++ app/org-catalog/page.tsx
@@ -2,6 +2,7 @@
 import { TypographyH2, TypographyP } from "@/components/ui/typography";
 import { redirect } from "next/navigation";
 import { useAuthContext } from "../(context)/auth-context";
+import OrgDetailDialog from "./org-detail-dialog";
 
 export default function OrgCatalog() {
   const { user } = useAuthContext();
@@ -20,6 +21,24 @@ export default function OrgCatalog() {
       <TypographyH2>Dashboard I edited this from renee</TypographyH2>
       <TypographyP>This is a protected route accessible only to signed-in users.</TypographyP>
       {user.email && <TypographyP>{`Your email is ${user.email}`}</TypographyP>}
+      <OrgDetailDialog
+        org={{
+          id: 'org_1',
+          name: 'Tech for Social Good',
+          description: 'Harvard Tech for Social Good (T4SG) leverages Harvard talent to \
+                        partner with nonprofits, government agencies, and social impact \
+                        organizations to amplify their impact through technology. Students \
+                        can develop their expertise in software engineering, user experience, \
+                        and ethics through semester-long projects with real-world clients.',
+          members: ['profile_1', 'profile_2', 'profile_3', 'profile_4', 'profile_5'],
+          directors: ['profile_1', 'profile_4'],
+          mailinglist: 'https://socialgood.hcs.harvard.edu/',
+          type: 'Technology',
+          comptype: 'Competitive',
+          meetingtime: 'Mondays 6-8 PM',
+          timecommitment: "7-10 hours per week"}}
+        cardEditsVisible={true}
+      />
     </>
   );
 }

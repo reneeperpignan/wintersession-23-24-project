3d6412b92f762bed73fa30730ec1719ef0ad9ec5
reneeperpignan <reneeperpignan3@gmail.com>
2024-01-17 14:14:12 -0500

    updating schema

--- app/org-catalog/org-detail-dialog.tsx
+++ app/org-catalog/org-detail-dialog.tsx
@@ -9,37 +9,75 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { TypographyP } from "@/components/ui/typography";
-import { Orgs } from "@/lib/firebase/schema";
-
+import { type Orgs } from "@/lib/firebase/schema";
 
 interface OrgDetailDialogProps {
-    org: Orgs;
-    cardEditsVisible: boolean;
+  org: Orgs;
+  cardEditsVisible: boolean;
 }
 
 export default function OrgDetailDialog({ org, cardEditsVisible }: OrgDetailDialogProps) {
-    return (
-        <Dialog>
-        <DialogTrigger asChild>
-            <Button variant="outline">Learn More</Button>
-        </DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-            <DialogTitle>{org.name}</DialogTitle>
-            <DialogDescription>
-                <TypographyP><b>Description:</b> {org.description}</TypographyP>
-                <TypographyP><b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
-                <TypographyP><b>Members:</b> {org.members.reduce((a: string, b: string) => a + ', ' + b)}</TypographyP>
-                <TypographyP><b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a></TypographyP>
-                <TypographyP><b>Type:</b> {org.type}</TypographyP>
-                <TypographyP><b>Comp Type:</b> {org.comptype}</TypographyP>
-                <TypographyP><b>Meeting Time:</b> {org.meetingtime}</TypographyP>               
-                <TypographyP><b>Time Commitment:</b> {org.timecommitment}</TypographyP>
-                {cardEditsVisible && <Button id="editButton" variant="outline" onClick={() => { alert('Edit button clicked') }}>Edit</Button>}
-                {cardEditsVisible && <Button id="deleteButton" variant="outline" onClick={() => { alert('Delete button clicked') }}>Delete</Button>}
-            </DialogDescription>
-            </DialogHeader>
-        </DialogContent>
-        </Dialog>
-    )
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Learn More</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{org.name}</DialogTitle>
+          <DialogDescription>
+            <TypographyP>
+              <b>Description:</b> {org.description}
+            </TypographyP>
+            <TypographyP>
+              <b>Directors:</b> {org.directors.reduce((a: string, b: string) => a + ", " + b)}
+            </TypographyP>
+            <TypographyP>
+              <b>Members:</b> {org.members.reduce((a: string, b: string) => a + ", " + b)}
+            </TypographyP>
+            <TypographyP>
+              <b>Mailing List:</b> <a href={org.mailinglist}>{org.mailinglist}</a>
+            </TypographyP>
+            <TypographyP>
+              <b>Type:</b> {org.type}
+            </TypographyP>
+            <TypographyP>
+              <b>Comp Type:</b> {org.comptype}
+            </TypographyP>
+            <TypographyP>
+              <b>Meeting Time:</b> {org.meetingtime}
+            </TypographyP>
+            <TypographyP>
+              <b>Time Commitment:</b> {org.timeupper}
+            </TypographyP>
+            <TypographyP>
+              <b>Time Commitment:</b> {org.timelower}
+            </TypographyP>
+            {cardEditsVisible && (
+              <Button
+                id="editButton"
+                variant="outline"
+                onClick={() => {
+                  alert("Edit button clicked");
+                }}
+              >
+                Edit
+              </Button>
+            )}
+            {cardEditsVisible && (
+              <Button
+                id="deleteButton"
+                variant="outline"
+                onClick={() => {
+                  alert("Delete button clicked");
+                }}
+              >
+                Delete
+              </Button>
+            )}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
